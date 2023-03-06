fb8a50762f75dc51a43d5a8c0ab0946c1d9372c7
Noah <89213023+chifouu65@users.noreply.github.com>
2023-03-06 22:25:23 +0100

    .

--- src/pages/api/auth/[nextauth].ts
+++ src/pages/api/auth/[nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
@@ -6,11 +6,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }),
-    ],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import {signIn, useSession} from 'next-auth/react'
 
-export default function Home() {
+function Home() {
   const { 
     data
    } = useSession()
@@ -16,3 +16,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home
